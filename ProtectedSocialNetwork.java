6a5bb67ee365484cb01026389e8973be04499e43
FabioBocci <noobgamingita@gmail.com>
2020-11-17 14:19:37 +0100

    add some implementations

--- ProtectedSocialNetwork.java
+++ ProtectedSocialNetwork.java
@@ -5,7 +5,7 @@ public interface ProtectedSocialNetwork extends SocialNetwork
 /**
  *          OVERVIEW:Estensione della classe SocialNetwork per la gestione di una rete sociale, con l'aggiunta di un controllo per segnalare i contenuti offensivi.
  * 
- *          TE: <ListOfPost , ListOfUser, ListOfBannedWord> AND f : user -> FollowersList t.c.
+ *          TE: <ListOfPost , ListOfUser, ListOfBannedWord> AND f : user -> FollowersList AND numLikes = g : Post -> Integer t.c.
  *                  (user ∈ ListOfUser => f(user) = FollowersList ⊂ ListOfUser) AND
  *                  ListOfPost != Null AND ListOfUser != Null AND 
  *                  (ListOfPost = {Post_1, Post_2, Post_3.......Post_n}) AND
@@ -13,15 +13,15 @@ public interface ProtectedSocialNetwork extends SocialNetwork
  *                  (forall ps1,ps2 in ListOfPost | ps1 != ps2 => ps1.ID != ps2.ID) AND
  *                  (forall Us1,Us2 in ListOfUser | Us1 != Us2 => Us1.Name != Us2.Name) AND
  *                  (forall user  in ListOfUser => f(user) != Null) AND
+ *                  (forall Post in ListOfPost => numLikes(Post) >= 0 AND numLikes(Post) <= #f(Post.author)) AND
  * 
- * 
- *                  (froall word in ListOfBannedWord => forall post in ListOfPost | post.getText().contains(word) == False )
+ *                  (froall word in ListOfBannedWord => NOT EXISTS post in ListOfPost | post.getText().contains(word) == True ) AND
+ *                  (forall word in ListOfBannedWord => NOT EXISTS User in ListOfUser | User.Name == word)
  */
     //Controlla che il post dato in input rispetti i vincoli sul contenuto (non abbia parole bannate)
     /**
-     * 
      * @param ps != Null
-     * @return True if (not Exists word in ListOfBannedWord | ps.getText().contain(word) == True ) else False
+     * @return True if (not Exists word in ListOfBannedWord | ps.getText().contain(word) == True OR ps.getAuthor() == word  ) else False
      * @throws NullPointerException if( ps == Null)
      */
     public boolean checkPost(Post ps)throws NullPointerException;
@@ -47,20 +47,57 @@ public interface ProtectedSocialNetwork extends SocialNetwork
     /**
      * 
      * @param words != Null AND ({words_1, words_2....words_n} | words_i !=  Null)
+     * @modify this.ListOfBannedWord and this.ListOfPost
+     * @effects POST(ListOfBannedWord) == words AND POST(ListOfPost) = PRE(ListOfPost) - ListOfPostBanned
      * @return  ListOfPostBanned is the list of post that contain one or more words in the new list, and got removed from this.ListOfPost
      * @throws NullPointerException If words == Null or contain null 
      */
     public List<Post> setBannedWords(List<String> words)throws NullPointerException;
 
     //aggiunge la parola Word alla lista delle parole bannate
-    public boolean addBannedWord(String Word)throws NullPointerException;
+    /**
+     * @param Word != Null
+     * @modify this.ListOfBannedWord and this.ListOfPost
+     * @effects POST(ListOfBannedWord) = PRE(ListOfBannedWord) ∪ Word AND POST(ListOfPost) = PRE(ListOfPost) - ListOfPostBanned
+     * @return ListOfPostBanned is the list of Post that contain the new Word and got removed to ListOfPost
+     * @throws NullPointerException if Word == Null
+     * @throws DoubleWordException if ListOfBannedWord.contains(Word) == True
+     */
+    public List<Post> addBannedWord(String Word)throws NullPointerException,DoubleWordException;
 
     //rimuove una parola dalla listad delle parole bannate
-    public boolean removeBannedWord(String Word)throws NullPointerException;
+    /**
+     * @param Word != Null AND ListOfBannedWord.contains(Word) == True
+     * @modify this.ListOfBannedWord
+     * @effects POST(ListOfBannedWord) = PRE(ListOfBannedWord) -  Word
+     * @return True if ListOfBannedWord don't contains Word anymore
+     * @throws NullPointerException if Word == Null
+     * @throws WordDontFoundException if ListOfBannedWord.contrains(Word) == False
+     */
+    public boolean removeBannedWord(String Word)throws NullPointerException,WordDontFoundException;
 
+    @Override
     //overide del metodo publicate post di SocialNetwork
+    /**
+     * @param ps != Null AND ps can't contains any word in ListOfBannedWord AND ListOfPost.contains(ps) == False
+     * @modify this.ListOfPost
+     * @effects POST(ListOfPost) = PRE(ListOfPost) ∪ ps
+     * @throws NullPointerException if ps == Null
+     * @throws IllegalArgumentException if ListOfPost.contains(ps) == True OR ps.getID() is in ListOfPost.ListOfIDs
+     * @throws IllegalWordsException if (Exists word in ListOfBannedWord | ps.getText().contains(word) == True )
+     */
     public void publicatePost(Post ps) throws NullPointerException, IllegalArgumentException ,IllegalWordsException;
+    
+    @Override
     //overide del metodo creare post di SocialNetwork
+    /**
+     * @param author != NUll 
+     * @param Text != Null AND Text.leght() < 141
+     * @return a new Post with Post.getAuthor() == author AND Post.getText() == Text AND ad ID that is not in the other POST
+     * @throws IllegalArgumentException if Text.leght() > 140
+     * @throws NullPointerException if Text == Null OR author != Null
+     * @throws IllegalWordsException if (Exists word in ListOfBannedWord | Text.contains(word) == True )
+     */
     public Post createPost(String author, String Text) throws IllegalArgumentException, NullPointerException,IllegalWordsException;
 
 
@@ -75,6 +112,28 @@ class IllegalWordsException extends RuntimeException{
     }
 
     public IllegalWordsException()
+    {
+        super();
+    }
+}
+
+class DoubleWordException extends RuntimeException{
+    public DoubleWordException(String word)
+    {
+        super(word);
+    }
+    public DoubleWordException()
+    {
+        super();
+    }
+}
+
+class WordDontFoundException extends RuntimeException{
+    public WordDontFoundException(String word)
+    {
+        super(word);
+    }
+    public WordDontFoundException()
     {
         super();
     }
--- SafeSocialNetwork.java
+++ SafeSocialNetwork.java
@@ -1,135 +1,306 @@
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class SafeSocialNetwork implements ProtectedSocialNetwork {
+/**
+ *              RI:
+ *              AF:
+ */
+
+    private Map<String, Set<String>> MapOfFollowers;
+    private Map<Post, Set<String>> MapOfLikes;
+    //private Map<String , Set<Post>> BannedWordsMap;
+    private Set<String> BannedWords;
+
+
+    public SafeSocialNetwork()
+    {
+        this.MapOfFollowers= new HashMap<>();
+        this.MapOfLikes = new HashMap<>();
+        this.BannedWords = new HashSet<>();
+    }
 
     @Override
     public Map<String, Set<String>> guessFollowers(List<Post> ps) throws NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(ps == null) throw new NullPointerException("Passato parametro Null a guessFollowers");
+        Set<String> authorSet = new HashSet<>();
+
+        for (Post post : ps) 
+        {
+            authorSet.add(post.getAuthor());    
+        }
+        Map<String , Set<String>> map = new HashMap<>();
+
+        for(String author : authorSet)
+        {
+            if(MapOfFollowers.keySet().contains(author))
+            {
+                map.put(author, MapOfFollowers.get(author));
+            }   
+        }
+        return map;
     }
 
     @Override
     public List<String> influencers() throws EmptyNetworkException {
-        // TODO Auto-generated method stub
-        return null;
+        if(MapOfFollowers.isEmpty()) throw new EmptyNetworkException("Network Empty");
+
+        SetValueComparato bvc = new SetValueComparato(MapOfFollowers);
+        TreeMap<String,Set<String>> sorted_map_by_value = new TreeMap<String, Set<String>>(bvc);    //la classe TreeMap esegue un ordinamento delle chiavi tramite il Comparator passato alla creazione
+        sorted_map_by_value.putAll(MapOfFollowers);         //all'inserimento verranno tutti ordinati
+
+       return new ArrayList<String>(sorted_map_by_value.keySet());
     }
 
     @Override
     public Set<String> getMentionedUsers() throws EmptyNetworkException {
-        // TODO Auto-generated method stub
-        return null;
+        if(MapOfFollowers.isEmpty() || MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+
+        Set<String> ListOfUser = MapOfFollowers.keySet();
+        Set<String> mentionedUser = new HashSet<>();
+        boolean found = false;
+
+        for (String user : ListOfUser) 
+        {
+            if(!mentionedUser.contains(user))
+            {
+                found = false;
+                for(Post ps : MapOfLikes.keySet())
+                {
+                    if(ps.checkWord(user)) found = true;
+                }
+                if(found) mentionedUser.add(user);
+            }
+        }
+        return mentionedUser;
     }
 
     @Override
     public Set<String> getMentionedUsers(List<Post> ps) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(MapOfFollowers.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(ps == null)throw new NullPointerException("getMentionedUsers got a Null");
+
+        Set<String> mentionedUsers = new HashSet<>();
+        int i;
+        boolean found;
+
+        for(String User : MapOfFollowers.keySet())
+        {
+            if(!mentionedUsers.contains(User))
+            {
+                found = false;
+                i=0;
+                while(!found && i < ps.size())
+                {
+                    found=ps.get(i).checkWord(User);
+                    i++;
+                }
+                if(found) mentionedUsers.add(User);
+            }
+        }
+
+        return mentionedUsers;
     }
 
     @Override
     public List<Post> writtenBy(String username) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(username == null) throw new NullPointerException();
+
+        List<Post> ps = new ArrayList<>();
+
+        for (Post p : MapOfLikes.keySet())
+        {
+            if(p.getAuthor()==username)
+                ps.add(p);
+        }
+
+        return ps;
     }
 
     @Override
     public List<Post> writtenBy(List<Post> ps, String username) throws NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(ps == null || username == null) throw new NullPointerException();
+
+        List<Post> p = new ArrayList<>();
+
+        for (Post post : ps) {
+            if(post.getAuthor() == username)
+                p.add(post);
+        }
+        return p;
     }
 
     @Override
     public List<Post> containing(List<String> words) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(words == null) throw new NullPointerException();
 
-    @Override
-    public List<Post> betweenDate(Date before, Date post)
-            throws EmptyNetworkException, IllegalArgumentException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        List<Post> tempList = new ArrayList<>(MapOfLikes.keySet());
+        List<Post> returnList = new ArrayList<>(MapOfLikes.keySet());
+        int i;
+        boolean found;
 
-    @Override
-    public List<Post> postDate(Date before) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        for (String word : words) 
+        {
+            i=0;
+            found=false;
+
+            while(!found && i<tempList.size())
+            {
+                if(tempList.get(i).checkWord(word))
+                {
+                    found=true;
+                    tempList.remove(i);
+                }
+            }
+        }
+        returnList.removeAll(tempList);
+
+        return returnList;
     }
 
     @Override
     public List<Post> removePosts(List<Post> ps) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(ps == null) throw new NullPointerException();
+        
+        for(Post p : ps)
+        {
+            if(MapOfLikes.keySet().contains(p))
+            {
+                MapOfLikes.remove(p);
+                ps.remove(p);
+            }
+        }
+        return ps;
     }
 
     @Override
     public boolean removePost(Post ps) throws EmptyNetworkException, NullPointerException {
-        // TODO Auto-generated method stub
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(ps == null) throw new NullPointerException();
+
+        if(MapOfLikes.remove(ps) != null)
+            return true;
         return false;
     }
 
     @Override
     public boolean removePostsbyAuthor(String username)
             throws EmptyNetworkException, NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return false;
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(username == null) throw new NullPointerException();
+        boolean foundOne = false;
+
+        for(Post pst : MapOfLikes.keySet())
+        {
+            if(pst.getAuthor()==username)
+            {
+                foundOne=true;
+                MapOfLikes.remove(pst);
+            }
+        }
+        if(foundOne)
+            return true;
+        
+        throw new IllegalArgumentException("User Don't Found");
     }
 
     @Override
-    public Set<String> followers(String username)
-            throws EmptyNetworkException, NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
+    public Set<String> followers(String username)throws EmptyNetworkException, NullPointerException, IllegalArgumentException {
+        if(MapOfLikes.isEmpty()) throw new EmptyNetworkException("Network Empty");
+        if(username == null) throw new NullPointerException();
+        if(!MapOfFollowers.keySet().contains(username)) throw new IllegalArgumentException("user non presente");
+
+        return new HashSet<>(MapOfFollowers.get(username));
     }
 
     @Override
     public boolean addFollower(String author, String username) throws IllegalArgumentException, NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+        if(author == null || username == null) throw new  NullPointerException();
+        if(author.equals(username)) throw new IllegalArgumentException("Nomi uguali, un utente non puo auto-seguirsi");
+        if(!MapOfFollowers.keySet().contains(author)) throw new IllegalArgumentException("Autore non presente");
+
+        if(!MapOfFollowers.keySet().contains(username)) createUser(username); //creo l'utente se non gia presente
+
+        if(MapOfFollowers.get(author).contains(username))
+            return false;
+        
+        MapOfFollowers.get(author).add(username);
+        return true;
     }
 
     @Override
     public void createUser(String User) throws NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
+        if(User == null) throw new NullPointerException();
+        if(MapOfFollowers.keySet().contains(User)) throw new IllegalArgumentException("Utente gia presente");
 
+        MapOfFollowers.put(User, new HashSet<String>());
+
+    }
+
+    @Override
+    public boolean addLike(Post ps, String user) throws NullPointerException, IllegalArgumentException {
+        if(ps == null ) throw new NullPointerException();
+        if(!MapOfLikes.keySet().contains(ps)) throw new IllegalArgumentException("Post non all'interno del SocialNetwork");
+        if(MapOfLikes.get(ps).contains(user)) return false;
+
+        MapOfLikes.get(ps).add(user);
+
+        return true;
     }
 
     @Override
     public boolean checkPost(Post ps) throws NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+        if(ps == null) throw new NullPointerException("post = Null in Check Post");
+        String text = ps.getText();
+        
+        for(String word : BannedWords)
+        {
+            if(text.contains(word)) return false;
+        }
+        return true;
     }
 
     @Override
     public List<Post> checkPosts(List<Post> pst) throws NullPointerException {
-        // TODO Auto-generated method stub
-        return null;
+        if(pst == null) throw new NullPointerException();
+        List<Post> newPostList = new ArrayList<>();
+
+        for(Post p : pst){
+            if(checkPost(p)) newPostList.add(p);
+        }
+        return newPostList;
     }
 
     @Override
     public List<String> getBannedWords() throws EmptyNetworkException {
-        // TODO Auto-generated method stub
-        return null;
+        if(BannedWords.isEmpty()) throw new EmptyNetworkException("Network emtpy");
+        return new ArrayList<>(BannedWords);
     }
 
     @Override
-    public List<Post> setBannedWords(List<String> pst) throws NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Post> setBannedWords(List<String> words) throws NullPointerException {
+        if(words == null) throw new NullPointerException("words = Null in setBannedWords");
+        BannedWords.clear();
+        BannedWords.addAll(words);
+        return checkRI();
     }
 
     @Override
-    public boolean addBannedWord(String Word) throws NullPointerException {
+    public List<Post> addBannedWord(String Word) throws NullPointerException, DoubleWordException {
         // TODO Auto-generated method stub
-        return false;
+        return checkRI();
     }
 
     @Override
-    public boolean removeBannedWord(String Word) throws NullPointerException {
+    public boolean removeBannedWord(String Word) throws NullPointerException, WordDontFoundException {
         // TODO Auto-generated method stub
         return false;
     }
@@ -141,9 +312,15 @@ public class SafeSocialNetwork implements ProtectedSocialNetwork {
     }
 
     @Override
-    public Post createPost(String author, String Text)
-            throws IllegalArgumentException, NullPointerException, IllegalWordsException {
+    public Post createPost(String author, String Text) throws IllegalArgumentException, NullPointerException, IllegalWordsException {
         // TODO Auto-generated method stub
+        return null;
+    }
+
+    //controlla il Rep. Invariant
+    //restituisce una lista di Post che non soddisfa più il RI, rimuovendoli da This.MapOfLikes.keySet()
+    List<Post> checkRI()
+    {
         return null;
     }
     
